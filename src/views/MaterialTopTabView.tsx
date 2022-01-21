935befe07741039ecc4fcf88dbd94c3b3251a613
Sourav Jangra <imsouravjangra@gmail.com>
2022-01-21 20:51:23 +0530

    gitpkg

---
+++ src/views/MaterialTopTabView.tsx
@@ -0,0 +1,82 @@
+import {
+  CommonActions,
+  ParamListBase,
+  Route,
+  TabNavigationState,
+  useTheme
+} from '@react-navigation/native';
+import * as React from 'react';
+import { View, ViewStyle } from 'react-native';
+import { SceneRendererProps, TabView } from 'react-native-tab-view';
+import type {
+  MaterialTopTabBarProps,
+  MaterialTopTabDescriptorMap,
+  MaterialTopTabNavigationConfig,
+  MaterialTopTabNavigationHelpers
+} from '../types';
+import MaterialTopTabBar from './MaterialTopTabBar';
+
+
+type Props = MaterialTopTabNavigationConfig & {
+  state: TabNavigationState<ParamListBase>;
+  navigation: MaterialTopTabNavigationHelpers;
+  descriptors: MaterialTopTabDescriptorMap;
+};
+
+export default function MaterialTopTabView({
+  tabBar = (props: MaterialTopTabBarProps) => <MaterialTopTabBar {...props} />,
+  state,
+  navigation,
+  descriptors,
+  sceneContainerStyle,
+  ...rest
+}: Props) {
+  const { colors } = useTheme();
+
+  const renderTabBar = (props: SceneRendererProps) => {
+    return tabBar({
+      ...props,
+      state: state,
+      navigation: navigation,
+      descriptors: descriptors,
+    });
+  };
+
+  const focusedOptions = descriptors[state.routes[state.index].key].options;
+  const tabBarStyle = focusedOptions.tabBarStyle as ViewStyle;
+
+  return (
+    <View style={{
+      flex:1,
+      backgroundColor: tabBarStyle.backgroundColor ?? colors.background
+    }}>
+      <TabView<Route<string>>
+      {...rest}
+      onIndexChange={(index) =>
+        navigation.dispatch({
+          ...CommonActions.navigate({
+            name: state.routes[index].name,
+            merge: true,
+          }),
+          target: state.key,
+        })
+      }
+      renderScene={({ route }) => descriptors[route.key].render()}
+      navigationState={state}
+      renderTabBar={renderTabBar}
+      renderLazyPlaceholder={({ route }) =>
+        descriptors[route.key].options.lazyPlaceholder?.() ?? null
+      }
+      lazy={({ route }) => descriptors[route.key].options.lazy === true}
+      lazyPreloadDistance={focusedOptions.lazyPreloadDistance}
+      swipeEnabled={focusedOptions.swipeEnabled}
+      onSwipeStart={() => navigation.emit({ type: 'swipeStart' })}
+      onSwipeEnd={() => navigation.emit({ type: 'swipeEnd' })}
+      sceneContainerStyle={[
+        { backgroundColor: colors.background },
+        sceneContainerStyle,
+      ]}
+    />
+    </View>
+  );
+}
